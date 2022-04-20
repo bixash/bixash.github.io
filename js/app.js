71ab13fb100bc6ea09c46a0bdcc63ec339ddf38d
Bixash <ibkstha@gmail.com>
2022-04-20 20:57:45 +0545

    updated about section
    added CV, some details

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 document.addEventListener("DOMContentLoaded", (event) =>  {
 
-var toggleMenu= document.getElementById("toggleMenu");
+    var toggleMenu= document.getElementById("toggleMenu");
 
-var closeMenu = document.getElementById("closeMenu");
+    var closeMenu = document.getElementById("closeMenu");
 
-var navOption = document.querySelector(".nav-option");
+    var navOption = document.querySelector(".nav-option");
+
+    var hireMe = document.getElementById("hire");
+
+    toggleMenu.addEventListener('click', ()=> {
+
+        navOption.style.display = "block";
+        
+    })
 
 
+    closeMenu.addEventListener('click', ()=> {
 
-toggleMenu.addEventListener('click', ()=> {
+        navOption.style.display = "none";
+        
+    })
 
-    navOption.style.display = "block";
     
-})
-closeMenu.addEventListener('click', ()=> {
-
-    navOption.style.display = "none";
-    
-})
 
 
 })
